ef108b186d3d07d9c519c1f72e63d2a8b512fd08
Jared Moscrip <jaredmoscrip@gmail.com>
2017-05-03 09:10:17 -0500

    beginnings

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-var mysql = require("mysql");
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-var connection = mysql.createConnection({
-	host: "localhost", 
-	port: 3306,
-	user: "root",
-	password: "IronMan",
-	database: "burgers_db"
+var app = express();
+var port = 3000;
+
+app.use(express.static(process.cwd() + "/views"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
 });
